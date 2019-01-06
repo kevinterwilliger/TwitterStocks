148bd2f162283e53ef39ca252149b8f270693cfd
kevinterwilliger <kevint3@illinois.edu>
2019-01-06 01:42:08 -0600

    initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+from tweetfeels import TweetFeels
+from tweetfeels import (TweetFeels, Tweet, TweetData, Sentiment)
+from threading import Thread
+import time
+import nltk
+
+consumer_key = "5l2zJ3Sp2COoTmYYRV4sgVald"
+consumer_secret = "g66DHjwKaNMu6DqI06woZW7AmWYRmBsR22lEpkLOiSIrgiJhBG"
+access_token = "967557596101083137-QEenqFrakqhHHNF9aq19YDqxyDsugw4"
+access_token_secret = "dIr7hWPw4orRwkjmNFziFWLEIZ4ifP70VJJrz9QO9eE9g"
+login = [consumer_key, consumer_secret, access_token, access_token_secret]
