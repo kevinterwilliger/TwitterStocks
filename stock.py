45a7610a518a9c4273efb466146c3d3448ca0424
kevinterwilliger <kevint3@illinois.edu>
2019-02-04 13:44:49 -0600

    stocks now pulled between trading hours

--- stock.py
+++ stock.py
@@ -1,13 +1,13 @@
 from iexfinance.stocks import Stock
 import sqlite3
 import os
-# import time
 import time
-from sentiment_puller import clear_dbs
+import datetime
+from sentiment import clear_dbs
 
 
 def pull_stocks() :
-    #i want it to wait until the stock market is open
+    #i want it to wait until the stock market is open, which i think i just did
     stock_list = ['AAPL', 'KO', 'DIS', 'MSFT', 'NKE',
                   'VZ', 'AMZN', 'TSLA']
     names_list = ['Apple', 'Coca_Cola', 'Walt_Disney', 'Microsoft', 'Nike',
@@ -25,23 +25,38 @@ def pull_stocks() :
         return
     finally :
         c = conn.cursor()
-        # rewrite for individual stock columns
         for name in names_list :
             string = "CREATE TABLE "+ name +" (date text, price real)"
             c.execute(string)
+        now = datetime.datetime.now()
+        open = now.replace(hour=8,minute=30,second=0,microsecond=0)
+        close = now.replace(hour=16,minute=0,second=0,microsecond=0)
         count = 0
-        while count < 5 :
-            print(f"starting cycle {count}")
-            for i in range(len(stock_list)) :
-                s = Stock(stock_list[i])
-                price = s.get_price()
-                date = str(time.ctime())
-                # this should work lmao
-                string = 'INSERT INTO ' + names_list[i] + ' VALUES (?,?)'
-                c.execute(string,(date,price))
-            conn.commit()
-            time.sleep(60)
-            count+=1
+        while True :
+            while now < close and now > open :
+                print(f"starting cycle {count}")
+                for i in range(len(stock_list)) :
+                    s = Stock(stock_list[i])
+                    price = s.get_price()
+                    date = str(time.ctime())
+                    string = 'INSERT INTO ' + names_list[i] + ' VALUES (?,?)'
+                    c.execute(string,(date,price))
+                conn.commit()
+                time.sleep(60)
+                count += 1
+                now = datetime.datetime.now()
+            if now > close :
+                o = datetime.timedelta(hours=open.hour + 24, minutes=open.minute)
+                n = datetime.timedelta(hours=now.hour,minutes=now.minute,seconds=now.second,microseconds=now.microsecond)
+                difference = o - n
+                print("Stock market closed, waiting " + str(difference.total_seconds()) + " seconds")
+                time.sleep(difference.total_seconds())
+            elif now < open :
+                o = datetime.timedelta(hours=open.hour,minutes=open.minute)
+                n = datetime.timedelta(hours=now.hour,minutes=now.minute,seconds=now.second,microseconds=now.microsecond)
+                difference = o - n
+                print("Stock market closed, waiting " + str(difference.total_seconds()) + " seconds")
+                time.sleep(difference.total_seconds())
 
 
 
